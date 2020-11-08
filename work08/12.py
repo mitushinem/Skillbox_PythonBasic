fa13a0aab0580333cc8dfbe95a7118aaca566fa1
Евгений Митюшин <e.mitushin@it-driver.ru>
2020-11-08 21:05:31 +0300

    Лекция 8. Задание 12

--- work08/12.py
+++ work08/12.py
@@ -1,12 +1,8 @@
-
 x = int(input('Дано число X: '))
 step1, step2, result_x1, result_x2 = 0, 1, 1, 1
 for _ in range(6):
     step1 = step1 * 2 + 1
-    result_x1 *= (x - step1)
-
-for _ in range(6):
     step2 = step2 * 2
+    result_x1 *= (x - step1)
     result_x2 *= (x - step2)
-
 print(result_x1/result_x2)
