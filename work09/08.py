5883e18baa2aedaf0b7f191fc5a3d7940abe9bcf
Евгений Митюшин <e.mitushin@it-driver.ru>
2020-11-19 11:34:33 +0300

    Лекция 9. доработки

--- work09/08.py
+++ work09/08.py
@@ -1,4 +1,19 @@
-name = input('Введите ФИ: ').split()
+# name = input('Введите ФИ: ').split()
+#
+# print('Фамилия:', name[0])
+# print('Имя:', name[1])
 
-print('Фамилия:', name[0])
-print('Имя:', name[1])
+
+fio = input('Введите ФИ: ')
+
+f, i = '', ''
+for symbol in fio:
+    if symbol not in ' ':
+        f += symbol
+    else:
+        break
+
+i = fio.replace(f,'').strip()
+
+print('Фамилия:', f)
+print('Имя:', i)
--- work09/10.py
+++ work09/10.py
@@ -1,8 +1,24 @@
-string = input('Введите текст: ').split()
-max_len = 0
+# string = input('Введите текст: ').split()
+# max_len = 0
+#
+# for s in string:
+#     if len(s) > max_len:
+#         max_len = len(s)
+#
+# print(f'Самое длинное слово состоит из {max_len} символов')
 
-for s in string:
-    if len(s) > max_len:
-        max_len = len(s)
+
+string = input('Введите текст: ')
+max_len, len_word = 0, 0
+str_tmp = string + ' '
+for s in str_tmp:
+    if s not in ' ':
+        len_word += 1
+    else:
+        if len_word > max_len:
+            max_len = len_word
+            len_word = 0
+        else:
+            len_word = 0
 
 print(f'Самое длинное слово состоит из {max_len} символов')
