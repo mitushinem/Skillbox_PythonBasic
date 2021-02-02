bb924e7954ad0b16881ecef1ac45896fabd86b43
e.mitushin <mitushinem@gmail.com>
2021-02-02 23:02:57 +0300

    Лекция 12. 9-10 задание

--- module12/task_9.py
+++ module12/task_9.py
@@ -1,13 +1,10 @@
 from random import randint
 
+
 def rock_paper_scissors():
     # игра "Камень, ножницы, бумага"
-    # Правила игры «Камень, ножницы, бумага»:
-    # программа запрашивает у пользователя строку и выводит, победил он или проиграл.
-    # Камень бьёт ножницы, ножницы режут бумагу, бумага кроет камень.
-
     while True:
-        action_computer = randint(1,4)
+        action_computer = randint(1, 4)
         action = int(input('1 - Камень, 2 - ножницы, 3 - бумага, 0 - Выход: '))
 
         if action == action_computer:
@@ -33,7 +30,7 @@ def rock_paper_scissors():
 
 def guess_the_number():
     # игра "Угадай число"
-    random_number = randint(1, 5)
+    random_number = randint(1, 10)
     while True:
         number = int(input('Введите число: '))
         if number == random_number:
@@ -42,15 +39,14 @@ def guess_the_number():
 
 
 def mainMenu():
-    game = int(input('1 - "Камень, ножницы, бумага", 2 - "Угадай число", 3 - Выход\nВыберите пункт меню: '))
     while True:
+        game = int(input('1 - "Камень, ножницы, бумага", 2 - "Угадай число", 3 - Выход\nВыберите пункт меню: '))
         if game == 1:
             rock_paper_scissors()
-
         elif game == 2:
             guess_the_number()
-
         elif game == 3:
             break
+
 
 mainMenu()
