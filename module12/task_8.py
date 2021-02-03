d95a7b2163257b8542a7d5fb53856278d7fcdc74
e.mitushin <mitushinem@gmail.com>
2021-02-03 15:37:21 +0300

    Лекция 12. 8 задание

--- module12/task_8.py
+++ module12/task_8.py
@@ -1,19 +1,17 @@
 
-def NOD(n1, n2):
+def is_prime(n1, n2):
     if n1 > n2:
-        num = n1
-        num_d = n2
+        min_n = n2
     else:
-        num = n2
-        num_d = n1
+        min_n = n1
 
-    ostatok = n1 % n2
-    n = num_d
-    while True:
-        tmp = n % ostatok
+    nod = 1
+    for i in range(2,min_n+1):
+        if n1 % i == 0 and n2 % i == 0:
+            nod = i
+    return nod
 
 
 n1 = int(input('Введите первое число: '))
 n2 = int(input('Введите второе число: '))
-
-print(f'НОД чисел {n1}, {n2} равно {NOD(n1, n2)}')
+print(f'НОД чисел {n1} и {n2} равно {is_prime(n1, n2)}')
