67c259e4b7a7cbafd212fe250e9525d53939c81c
e.mitushin <mitushinem@gmail.com>
2021-02-05 17:46:59 +0300

    Лекция 12. 6,10 задание

--- module12/task_6.py
+++ module12/task_6.py
@@ -1,11 +1,7 @@
 
 
 def find_money(x, y):
-    from random import uniform
-    x_1 = round(uniform(-10, 10), 3)
-    y_1 = round(uniform(-10, 10), 3)
-    print(x_1, y_1)
-    if (x_1 - 1 <= x <= x_1 + 1) and (y_1 - 1 <= y <= y_1 + 1):
+    if (-1 <= x <= 1) and (- 1 <= y <= 1):
         print('Монетка рядом')
     else:
         print('Монетка не найдена')
